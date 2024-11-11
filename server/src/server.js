0fc6e439ad26a75f6630f8d2b2fa3a2a02503f9b
BuraYu <burak.yueksel@hotmail.com>
2024-11-11 22:53:08 +0100

    restructured the server MVC model

--- server/src/server.js
+++ server/src/server.js
@@ -1,36 +1,11 @@
 import express from "express";
-import { StatusCodes } from "http-status-codes";
+import appRoutes from "./routes";
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
-
-const STATUS = {
-  SUCCESS: "OK",
-  FAILURE: "NO",
-};
-
-app.get("/", (req, res) => {
-  res.status(StatusCodes.NOT_FOUND);
-  res.send("Hello You");
-});
-
-app.post("/add", (req, res) => {
-  const data = [];
-  const { body } = req;
-  if (!body.name) {
-    return res.status(StatusCodes.BAD_REQUEST).send({
-      status: STATUS.FAILURE,
-      message: "Name is required",
-    });
-  }
-  data.push(req.body);
-  return res.status(StatusCodes.CREATED).send({
-    status: STATUS.SUCCESS,
-    message: data,
-  });
-});
+app.use("/v1", appRoutes);
 
 app.listen(port, () => {
   console.log(`Hey, go to localhost: ${port}`);
