51816ed63559239633e344a755ae666babb5d7af
BuraYu <burak.yueksel@hotmail.com>
2024-11-12 19:50:13 +0100

    moved ping to own file

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,14 @@
 import express from "express";
-import appRoutes from "./users.routes";
+import userRoutes from "./users.routes";
+import mainRoutes from "./main.routes";
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.use("/v1", appRoutes);
+
+app.use("/v1", mainRoutes);
+app.use("/v1/user", userRoutes);
 
 app.listen(port, () => {
   console.log(`Hey, go to localhost: ${port}`);
